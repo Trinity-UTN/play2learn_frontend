baffc48f514e02b4561bc355dba92de21f5cbb11
JaaF97 <julianfassetta@gmail.com>
2025-06-24 11:22:15 -0300

    feat: Crear instancia de axios

---
+++ src/shared/utils/api.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://play2learn.backend.desarrollo.systemsbinary.com";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
