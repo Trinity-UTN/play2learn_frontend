4ffc30a0d57d44b352d436d6f22dcab457addc40
JaaF97 <julianfassetta@gmail.com>
2025-07-09 00:43:55 -0300

    feat: Implementar listado de years (sin paginación) y eliminación de years. #39

--- src/admin/contexts/yearContext/YearContext.type.ts
+++ src/admin/contexts/yearContext/YearContext.type.ts
@@ -1,11 +1,17 @@
 import type {
   CreateYearPayload,
   GetYearPayload,
-} from "../../services/Year/YearService";
+  GetPaginatedYearPayload,
+  PaginatedData,
+  YearResponseDto,
+} from "../../services/year/YearService";
 
 export interface YearContextType {
   loading: boolean;
   registerYear: (data: CreateYearPayload) => Promise<void>;
   getYear: () => void;
+  getPaginatedYear: (params: GetPaginatedYearPayload) => Promise<void>;
+  deleteYear: (id: number) => Promise<void>;
   years: GetYearPayload[];
+  paginatedYears: PaginatedData<YearResponseDto> | null;
 }
--- src/admin/contexts/yearContext/YearProvider.tsx
+++ src/admin/contexts/yearContext/YearProvider.tsx
@@ -1,11 +1,14 @@
 import { useState, type ReactNode } from "react";
 import { YearContext } from "./YearContext";
 import type { YearContextType } from "./YearContext.type";
-import { YearService } from "../../services/Year/YearService";
+import { YearService } from "../../services/year/YearService";
 import type {
   CreateYearPayload,
   GetYearPayload,
-} from "../../services/Year/YearService";
+  GetPaginatedYearPayload,
+  PaginatedData,
+  YearResponseDto,
+} from "../../services/year/YearService";
 
 interface YearProviderProps {
   children: ReactNode;
@@ -14,6 +17,8 @@ interface YearProviderProps {
 export const YearProvider: React.FC<YearProviderProps> = ({ children }) => {
   const [loading, setLoading] = useState<boolean>(false);
   const [years, setYears] = useState<GetYearPayload[]>([]);
+  const [paginatedYears, setPaginatedYears] =
+    useState<PaginatedData<YearResponseDto> | null>(null);
 
   const registerYear = async (data: CreateYearPayload): Promise<void> => {
     setLoading(true);
@@ -40,11 +45,41 @@ export const YearProvider: React.FC<YearProviderProps> = ({ children }) => {
     }
   };
 
+  const getPaginatedYear = async (
+    params: GetPaginatedYearPayload
+  ): Promise<void> => {
+    setLoading(true);
+    try {
+      const response = await YearService.getPaginatedYearApi(params);
+      setPaginatedYears(response.data);
+    } catch (error) {
+      console.error("Error al obtener los años paginados:", error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteYear = async (id: number): Promise<void> => {
+    setLoading(true);
+    try {
+      await YearService.deleteYearApi(id);
+    } catch (error) {
+      console.error("Error al eliminar el año:", error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const contextValue: YearContextType = {
     loading,
     registerYear,
     getYear,
+    getPaginatedYear,
+    deleteYear,
     years,
+    paginatedYears,
   };
 
   return (
