c93f7aef027e3d70afde2d75d70137af6dcfb27b
JaaF97 <julianfassetta@gmail.com>
2025-07-18 10:26:54 -0300

    feat: Agregar lógica de get/getPaginated/delete subject. #41

--- src/admin/contexts/subjectContext/SubjectContext.type.ts
+++ src/admin/contexts/subjectContext/SubjectContext.type.ts
@@ -1,6 +1,18 @@
-import type { CreateSubjectPayload } from "../../services/subject/SubjectService";
+import type {
+  GetPaginated,
+  PaginatedData,
+} from "../../../shared/types/PaginacionType";
+import type {
+  CreateSubjectPayload,
+  SubjectResponseDto,
+} from "../../services/subject/SubjectService";
 
 export interface SubjectContextType {
   loading: boolean;
   registerSubject: (data: CreateSubjectPayload) => Promise<void>;
+  getSubject: () => void;
+  getPaginatedSubject: (params: GetPaginated) => Promise<void>;
+  deleteSubject: (id: number) => Promise<void>;
+  subjects: SubjectResponseDto[];
+  paginatedSubjects: PaginatedData<SubjectResponseDto> | null;
 }
--- src/admin/contexts/subjectContext/SubjectProvider.tsx
+++ src/admin/contexts/subjectContext/SubjectProvider.tsx
@@ -1,8 +1,15 @@
-import { useState, type ReactNode } from "react";
+import { useCallback, useState, type ReactNode } from "react";
+import type {
+  GetPaginated,
+  PaginatedData,
+} from "../../../shared/types/PaginacionType";
 import { SubjectContext } from "./SubjectContext";
 import type { SubjectContextType } from "./SubjectContext.type";
 import { SubjectService } from "../../services/subject/SubjectService";
-import type { CreateSubjectPayload } from "../../services/subject/SubjectService";
+import type {
+  CreateSubjectPayload,
+  SubjectResponseDto,
+} from "../../services/subject/SubjectService";
 
 interface SubjectProviderProps {
   children: ReactNode;
@@ -12,6 +19,9 @@ export const SubjectProvider: React.FC<SubjectProviderProps> = ({
   children,
 }) => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [subjects, setSubjects] = useState<SubjectResponseDto[]>([]);
+  const [paginatedSubjects, setPaginatedSubjects] =
+    useState<PaginatedData<SubjectResponseDto> | null>(null);
 
   const registerSubject = async (data: CreateSubjectPayload): Promise<void> => {
     setLoading(true);
@@ -25,9 +35,55 @@ export const SubjectProvider: React.FC<SubjectProviderProps> = ({
     }
   };
 
+  const getSubject = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await SubjectService.getSubjectApi();
+      setSubjects(response.data.data);
+    } catch (error) {
+      console.error("Error al obtener las materias:", error); // TODO: REMOVE_DEBUG
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const getPaginatedSubject = useCallback(
+    async (params: GetPaginated): Promise<void> => {
+      setLoading(true);
+      try {
+        const response = await SubjectService.getPaginatedSubjectApi(params);
+        setPaginatedSubjects(response.data);
+      } catch (error) {
+        console.error("Error al obtener las materias paginadas:", error); // TODO: REMOVE_DEBUG
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
+
+  const deleteSubject = async (id: number): Promise<void> => {
+    setLoading(true);
+    try {
+      await SubjectService.deleteSubjectApi(id);
+    } catch (error) {
+      console.error("Error al eliminar la materia:", error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const contextValue: SubjectContextType = {
     loading,
     registerSubject,
+    getSubject,
+    getPaginatedSubject,
+    deleteSubject,
+    subjects,
+    paginatedSubjects,
   };
 
   return (
