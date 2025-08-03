5a0393fce6ce314864600e04abb2ae481f33798f
Julián Fassetta <132026269+JulianFassetta@users.noreply.github.com>
2025-08-03 15:37:57 -0300

    fix: espaciado en mapeado de actividades. #51

--- src/activity/utils/activityComponentMap.ts
+++ src/activity/utils/activityComponentMap.ts
@@ -2,6 +2,7 @@ import CreateAhorcado from "../views/createAhorcadoView/CreateAhorcado";
 import CreateCompletarOracion from "../views/createCompletarOracionView/CreateCompletarOracion";
 import CreatePreguntados from "../views/createPreguntadosView/CreatePreguntados";
 import CreateSequenceView from "../views/createSequenceView/CreateSequenceView";
+
 export const activityComponentMap: Record<string, React.ComponentType> = {
   ahorcado_educativo: CreateAhorcado,
   completar_oraciones: CreateCompletarOracion,
