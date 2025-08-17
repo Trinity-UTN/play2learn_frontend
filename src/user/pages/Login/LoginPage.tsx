9466ec0ef4f28b39c9f28e2bf1b2942ca784e56c
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2025-08-17 18:34:29 -0300

    Eliminacion de un icono que no se usaba

--- src/user/pages/Login/LoginPage.tsx
+++ src/user/pages/Login/LoginPage.tsx
@@ -2,13 +2,7 @@ import type React from "react";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import {
-  FaGraduationCap,
-  FaEye,
-  FaEyeSlash,
-  FaUser,
-  FaLock,
-} from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaUser, FaLock } from "react-icons/fa";
 import { useAuth } from "../../hooks/useAuth";
 import Button from "../../../shared/components/Button/ButtonComponent";
 import Input from "../../../shared/components/Input/InputComponent";
