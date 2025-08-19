8c66e30943ee2a424a91c5228eda59e5f5772cbb
JaaF97 <julianfassetta@gmail.com>
2025-08-19 20:44:54 -0300

    fix: Tipos de AvatarComponents. #100

--- src/student/types/CurrentStudent.type.ts
+++ src/student/types/CurrentStudent.type.ts
@@ -2,8 +2,8 @@ import type { StudentResponseDto as Student } from "../../admin/services/student
 
 export interface CurrentStudent extends Student {}
 
-export interface UpdateProfilePayload {
-  selectedBody?: number | null;
-  selectedShirt?: number | null;
-  selectedHat?: number | null;
+export interface AvatarComponents {
+  body: string;
+  shirt: string;
+  hat: string;
 }
