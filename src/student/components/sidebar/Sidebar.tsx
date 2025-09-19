279b083766d235a65e245455f03f1f5b4c9c2f20
JaaF97 <julianfassetta@gmail.com>
2025-09-19 12:08:13 -0300

    fix: Se agrego pendingCount a sidebar. #201

--- src/student/components/sidebar/Sidebar.tsx
+++ src/student/components/sidebar/Sidebar.tsx
@@ -39,7 +39,7 @@ const StudentSidebar: React.FC<StudentSidebarProps> = ({
 }) => {
   const { logout } = useAuth();
   const { wallet, currentStudent } = useCurrentStudent();
-  const { availableCount } = useActivityStudentUI();
+  const { pendingCount } = useActivityStudentUI();
   const navigate = useNavigate();
 
   const menuItems: MenuItem[] = [
@@ -55,9 +55,7 @@ const StudentSidebar: React.FC<StudentSidebarProps> = ({
       icon: FaWallet,
       path: StudentRoutes.Wallet,
       color: "#10B981",
-      badge: isLoading
-        ? "..."
-        : formatPrice(wallet?.balance) ?? "0",
+      badge: isLoading ? "..." : formatPrice(wallet?.balance) ?? "0",
     },
     {
       title: "Mis Actividades",
