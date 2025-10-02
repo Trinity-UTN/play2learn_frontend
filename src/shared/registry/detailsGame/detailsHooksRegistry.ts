a6989a4b71a179dc80975bdd0addffe0789f8e26
JaaF97 <julianfassetta@gmail.com>
2025-10-01 23:22:39 -0300

    refactor: Agregar componentes a registry de detalles para recompensa de actividad. #167

--- src/shared/registry/detailsGame/detailsHooksRegistry.ts
+++ src/shared/registry/detailsGame/detailsHooksRegistry.ts
@@ -1,9 +1,14 @@
 import { GameType } from "../../types/Games.type";
 import DetailsAhorcado from "../../../student/components/DetailsGame/DetailsAhoracado/DetailsAhorcado";
+import DetailsCompletarOracionGame from "../../../student/components/DetailsGame/DetailsCompletarOracion/DetailsCompletarOracionGame";
 import DetailsDesafioClasificacionGame from "../../../student/components/DetailsGame/DetailsDesafioClasificacion/DetailsDesafioClasificacionGame";
 
 export const GAME_TYPE_TO_COMPONENT: Record<GameType, React.FC<any>> = {
   [GameType.AHORCADO]: DetailsAhorcado,
   [GameType.CLASIFICACION]: DetailsDesafioClasificacionGame,
+  [GameType.COMPLETAR_ORACION]: DetailsCompletarOracionGame,
+  [GameType.MEMORAMA]: DetailsDesafioClasificacionGame,
+  [GameType.NO_LUDICA]: DetailsDesafioClasificacionGame,
+  [GameType.ORDENAR_SECUENCIA]: DetailsDesafioClasificacionGame,
   [GameType.PREGUNTADOS]: DetailsDesafioClasificacionGame,
 };
