09665d84a386082a0b1505993db4ee9ab3ca59ae
B3RZVS <79112396+B3RZVS@users.noreply.github.com>
2026-06-11 17:32:59 -0300

    fix: overview por profile

--- src/student/components/sidebar/Sidebar.tsx
+++ src/student/components/sidebar/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { motion, type Variants } from "framer-motion";
 import {
-  FaHome,
   FaWallet,
   FaGamepad,
   FaGift,
@@ -48,13 +47,6 @@ const StudentSidebar: React.FC<StudentSidebarProps> = ({
   const availableBenefitCount = benefitStats?.available ?? 0;
 
   const menuItems: MenuItem[] = [
-    {
-      title: "Panel Principal",
-      icon: FaHome,
-      path: StudentRoutes.Overview,
-      color: "#3B82F6",
-      badge: "",
-    },
     {
       title: "Mi Billetera",
       icon: FaWallet,
@@ -159,7 +151,7 @@ const StudentSidebar: React.FC<StudentSidebarProps> = ({
           <div className={styles.buttonSkeleton}></div>
         ) : (
           <button className={styles.buttonPerfil} onClick={handleProfileClick}>
-            Ver Perfil
+            Panel Principal
           </button>
         )}
       </motion.div>
