3b3acf4ed4060dc9626d415036263d16560deb61
JulianFassetta <julianfassetta@gmail.com>
2026-06-12 16:16:40 -0300

    refactor: Filtros de actividades en teacher y student. #318

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,3 +2,4 @@ export { SUBJECT_COLORS, getSubjectColor } from "./subject.constants";
 export { containerVariants, itemVariants } from "./variants.constants";
 export { FILTER_TYPES } from "./filters.contanst";
 export * from "./games.constants";
+export * from "./activity.constants";
--- src/student/hooks/activities/activityList/useActivityFilters.ts
+++ src/student/hooks/activities/activityList/useActivityFilters.ts
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import type { FilterOption } from "@/shared";
+import { ACTIVITY_NAME_ALL, type FilterOption } from "@/shared";
 import type { ActivityStatus } from "../../../constants/activities.constants";
 
 export const useActivityFilters = () => {
@@ -9,20 +9,25 @@ export const useActivityFilters = () => {
     name: "Todas las materias",
   });
   const [selectedDifficulty, setSelectedDifficulty] = useState<string>("ALL");
+  const [selectedActivityName, setSelectedActivityName] =
+    useState<string>(ACTIVITY_NAME_ALL);
 
   const resetFilters = useCallback(() => {
     setActiveFilter("PUBLISHED");
     setSelectedSubject({ id: "ALL", name: "Todas las materias" });
     setSelectedDifficulty("ALL");
+    setSelectedActivityName(ACTIVITY_NAME_ALL);
   }, []);
 
   return {
     activeFilter,
     selectedSubject,
     selectedDifficulty,
+    selectedActivityName,
     setActiveFilter,
     setSelectedSubject,
     setSelectedDifficulty,
+    setSelectedActivityName,
     resetFilters,
   };
 };
